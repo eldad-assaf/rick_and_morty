f6b3baef87444374f1d20bbe7482f28cf534c5f6
eldad-assaf <eldadux@gmail.com>
2023-03-16 12:23:36 +0200

    showing characters

--- lib/views/screens/main_screen.dart
+++ lib/views/screens/main_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rick_and_morty/state/bloc/character_bloc.dart';
-import 'package:rick_and_morty/state/repository/characters_repository.dart';
+
+import '../../state/models/character_model.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -18,7 +19,7 @@ Widget blocBody(BuildContext context) {
   return BlocBuilder<CharacterBloc, CharacterState>(
     builder: (context, state) {
       if (state is LoadingCharactersState) {
-        return Center(child: CircularProgressIndicator());
+        return const Center(child: CircularProgressIndicator());
       }
       if (state is CharactersErrorState) {
         return Center(
@@ -26,9 +27,49 @@ Widget blocBody(BuildContext context) {
         );
       }
       if (state is CharactersLoadedState) {
-        return Center(child: Text('loaded'));
+        return GridView.count(
+          crossAxisCount: 2, // Show 2 characters per row
+          childAspectRatio: 0.75, // Set the aspect ratio of each grid item
+          children: state.characters.map((character) {
+            return CharacterItemWidget(character: character);
+          }).toList(),
+        );
       }
       return Container();
     },
   );
+}
+
+class CharacterItemWidget extends StatelessWidget {
+  final Character character;
+
+  const CharacterItemWidget({Key? key, required this.character})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Image.network(
+              character.image,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              character.name,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
