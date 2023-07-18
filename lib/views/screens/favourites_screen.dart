1e33f914119835e3f76792ec76b5ac45d71bae5d
eldad assaf <eldadux@gmail.com>
2023-07-18 19:43:32 +0300

    done

--- lib/views/screens/favourites_screen.dart
+++ lib/views/screens/favourites_screen.dart
@@ -4,6 +4,9 @@ import 'package:rick_and_morty/state/blocs/favourites_blocs/favourites_bloc.dart
 import 'package:rick_and_morty/state/models/character_model.dart';
 import 'package:rick_and_morty/views/widgets/favourites_list_grid_view.dart';
 
+import '../../common/utils/constants.dart';
+import '../../common/utils/text_style.dart';
+
 class FavouritesScreen extends StatelessWidget {
   const FavouritesScreen({super.key});
 
@@ -12,16 +15,22 @@ class FavouritesScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
+          elevation: 0,
+          iconTheme: const IconThemeData(color: Appconst.kLight),
+          backgroundColor: Appconst.kBkDark,
           centerTitle: true,
-          title: const Text('Favourites'),
+          title: Text(
+            'Favourites',
+            style: appStyle(25, Appconst.kLight, FontWeight.bold),
+          ),
         ),
         body: BlocBuilder<FavouritesBloc, List<Character>>(
           builder: (context, state) {
             if (state.isEmpty) {
-              return const Center(
+              return Center(
                 child: Text(
                   'there are no favourites yet.',
-                  style: TextStyle(fontSize: 22),
+                  style: appStyle(20, Appconst.kLight, FontWeight.normal),
                 ),
               );
             } else {
